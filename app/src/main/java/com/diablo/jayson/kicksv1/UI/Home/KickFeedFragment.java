2e547e8a539af69f93409a758ffcc33bd9e95960
CerealThenMilk <jaysonamati@gmail.com>
2020-02-29 14:15:54 +0300

    cleaned up unused stuff

--- app/src/main/java/com/diablo/jayson/kicksv1/UI/Home/KickFeedFragment.java
+++ app/src/main/java/com/diablo/jayson/kicksv1/UI/Home/KickFeedFragment.java
@@ -1,6 +1,5 @@
 package com.diablo.jayson.kicksv1.UI.Home;
 
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +22,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class KickFeedFragment extends Fragment {
     private static final String TAG = AddKickFragment.class.getSimpleName();
@@ -41,95 +39,20 @@ public class KickFeedFragment extends Fragment {
         View root = inflater.inflate(R.layout.kick_feed_fragment, container, false);
         mRecyclerView = root.findViewById(R.id.recyclerview);
         mRelativelayout = root.findViewById(R.id.searchAndProfileRelativeView);
-//        initializeData();
-        dissappearItemsOnScroll();
         loadActivitiesFromFirebase();
 
         return root;
 
     }
 
-    private void initializeData() {
-        String[] kicksTitles = getResources()
-                .getStringArray(R.array.kicks_titles);
-        String[] kicksDates = getResources()
-                .getStringArray(R.array.kicks_dates);
-        String[] kicksTimes = getResources()
-                .getStringArray(R.array.kicks_time);
-        String[] kicksLocations = getResources()
-                .getStringArray(R.array.kicks_location);
-        String[] alreadyAttending = getResources()
-                .getStringArray(R.array.already_attending_peeps);
-        String[] requiredAttending = getResources()
-                .getStringArray(R.array.required_peeps);
-        TypedArray kicksImageResources = getResources().obtainTypedArray(R.array.kicks_images);
-
-        mKicksData.clear();
-
-        String[] tagsList = {"Bowling", "Skating"};
-
-        for (int i = 0; i < kicksTitles.length; i++) {
-            mKicksData.add(new Activity(kicksTitles[i], kicksTimes[i], kicksDates[i],
-                    kicksLocations[i], alreadyAttending[i], requiredAttending[i],
-                    "", Arrays.asList(tagsList)));
-        }
-        kicksImageResources.recycle();
-
-        mAdapter.notifyDataSetChanged();
-    }
 
     private void loadActivitiesFromFirebase() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference documentReference = db.collection("activities").document();
-//        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                Activity activity = documentSnapshot.toObject(Activity.class);
-//                mKicksData = new ArrayList<Activity>();
-//                mKicksData.add(activity);
-//                initializeRecyclerView();
-//            }
-//        });
-//        db.collection("activities")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                Log.e(TAG, document.getId() + " => " + document.getData());
-//                                for (int i=0;i<document.getData().size();i++) {
-//                                    Activity activity = document.toObject(Activity.class);
-//                                    mKicksData = new ArrayList<Activity>();
-//                                    mKicksData.add(activity);
-//                                    initializeRecyclerView();
-//                                }
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-
         Query query = FirebaseFirestore.getInstance()
                 .collection("activities")
                 .whereGreaterThan("imageUrl", "");
-//        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot documentSnapshots, @Nullable FirebaseFirestoreException e) {
-//                if (e != null){
-//
-//                }
-//
-//                assert documentSnapshots != null;
-//
-//                ArrayList<Activity> activities = (ArrayList<Activity>) documentSnapshots.toObjects(Activity.class);
-//                int gridColumnCount = 1;
-//                mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), gridColumnCount));
-//                mAdapter = new ActivityFeedListAdapter(getContext(), activities);
-//                mRecyclerView.setAdapter(mAdapter);
-//            }
-//        });
+
 
         FirestoreRecyclerOptions<Activity> options = new FirestoreRecyclerOptions.Builder<Activity>()
                 .setQuery(query, Activity.class)
@@ -140,66 +63,6 @@ public class KickFeedFragment extends Fragment {
         mRecyclerView.setAdapter(mAdapter);
 
 
-//        FirestoreRecyclerAdapter adapter = new FirestoreRecyclerAdapter<Activity, ActivityFeedListAdapter.ActivityViewHolder>(options) {
-//            @Override
-//            protected void onBindViewHolder(@NonNull ActivityFeedListAdapter.ActivityViewHolder viewHolder, int positions, Activity model) {
-//                bindViewHolder(viewHolder,positions);
-//            }
-//
-//            @Override
-//            public ActivityFeedListAdapter.ActivityViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//                View view = LayoutInflater.from(parent.getContext())
-//                        .inflate(R.layout.activity_list_item,parent,false);
-//                return new ActivityFeedListAdapter.ActivityViewHolder(view);
-//            }
-//        };
-//        int gridColumnCount = 1;
-//        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), gridColumnCount));
-//        mRecyclerView.setAdapter(adapter);
-
-
-//        db.collection("activities")
-////                .whereEqualTo("imageUrl","ww")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()){
-//                            for (QueryDocumentSnapshot document: Objects.requireNonNull(task.getResult())){
-//                                Log.e(TAG,document.getId()+" => " + document.getData());
-//                                Activity activity = document.toObject(Activity.class);
-//                                mKicksData = new ArrayList<Activity>();
-//                                mKicksData.add(activity);
-//
-//                            }
-//                            initializeRecyclerView();
-//                        }else {
-//                            Log.d(TAG, "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-    }
-
-//    private void initializeRecyclerView() {
-//        int gridColumnCount = 1;
-//        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), gridColumnCount));
-//        mAdapter = new ActivityFeedListAdapter(getContext(), mKicksData);
-//        mRecyclerView.setAdapter(mAdapter);
-//    }
-
-    private void dissappearItemsOnScroll() {
-//        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//            @Override
-//            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-//            }
-//
-//            @Override
-//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-//                super.onScrolled(recyclerView, dx, dy);
-//                mRelativelayout.setVisibility(View.INVISIBLE);
-//            }
-//        });
     }
 
     @Override
