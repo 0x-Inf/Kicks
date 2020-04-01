f82b83ece1958e7bf30a42d54c305132fd064854
CerealThenMilk <jaysonamati@gmail.com>
2020-04-01 21:30:33 +0300

    Android Studio issues

--- app/src/main/java/com/diablo/jayson/kicksv1/UI/Search/SearchableActivity.java
+++ app/src/main/java/com/diablo/jayson/kicksv1/UI/Search/SearchableActivity.java
@@ -4,8 +4,17 @@ import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.diablo.jayson.kicksv1.Models.Tag;
 import com.diablo.jayson.kicksv1.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 public class SearchableActivity extends ListActivity {
 
@@ -19,9 +28,44 @@ public class SearchableActivity extends ListActivity {
             String query = intent.getStringExtra(SearchManager.QUERY);
             doMySearch(query);
         }
+
+
     }
 
     private void doMySearch(String query) {
+    }
 
+
+    public class TagSearchListAdapter extends FirestoreRecyclerAdapter<Tag, TagSearchListAdapter.SearchedTagViewHolder> {
+
+        /**
+         * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+         * FirestoreRecyclerOptions} for configuration options.
+         *
+         * @param options
+         */
+        public TagSearchListAdapter(@NonNull FirestoreRecyclerOptions options) {
+            super(options);
+        }
+
+
+        @NonNull
+        @Override
+        public SearchedTagViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new SearchedTagViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.searched_tag_item, parent, false));
+        }
+
+        @Override
+        protected void onBindViewHolder(@NonNull SearchedTagViewHolder holder, int position, @NonNull Tag model) {
+
+        }
+
+        class SearchedTagViewHolder extends RecyclerView.ViewHolder {
+
+            public SearchedTagViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+        }
     }
 }
