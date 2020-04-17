bddfe27fdcbfe1819a10b5a1b928baf2811002da
Jayson Amati <48046828+imakedepressedbots@users.noreply.github.com>
2020-04-18 01:25:48 +0300

    Changed the gitignore files again

--- app/src/main/java/com/diablo/jayson/kicksv1/ApiThings.java
+++
@@ -1,7 +0,0 @@
-package com.diablo.jayson.kicksv1;
-
-public class ApiThings {
-
-    public static final String places_api_key = "AIzaSyDrZtRYNPGMye467hX4Y0SWmkTp9mSUpCs";
-    public static final String geocoder_api_key = "AIzaSyDO4tBIzu11B7fbreXfpf-P7Rsxx0zBBWM";
-}
